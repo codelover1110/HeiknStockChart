7ecc8bd7b25b06843b1ed11d66dab28f9de371a9
whitesnow1120 <skybearok@outlook.com>
2021-09-21 02:10:09 +0300

    conncted to the backend for datatable

--- chart_frontend/src/components/FinancialDashboard/FinancialDataTable.js
+++ chart_frontend/src/components/FinancialDashboard/FinancialDataTable.js
@@ -1,96 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import Select from 'react-select';
 import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
 
 const FinancialDataTable = (props) => {
-  const { data, symbols } = props;
+  const { data } = props;
 
-  const [symbol, setSymbol] = useState({ value: 'GOOG', label: 'GOOG' });
-  const [optionsSymbol, setOptionsSymbol] = useState([]);
   const [datatable, setDatatable] = useState();
+  const [sorted, setSorted] = useState(0);
 
-  // const hearder_columns = [
-  //   {
-  //     label: 'O',
-  //     field: 'o',
-  //     width: 200,
-  //     attributes: {
-  //       'aria-controls': 'DataTable',
-  //       'aria-label': 'symbol',
-  //     },
-  //   },
-  //   {
-  //     label: 'H',
-  //     field: 'h',
-  //     width: 200,
-  //   },
-  //   {
-  //     label: 'C',
-  //     field: 'c',
-  //     width: 200,
-  //   },
-  //   {
-  //     label: 'L',
-  //     field: 'l',
-  //     width: 200,
-  //   },
-  //   {
-  //     label: 'V',
-  //     field: 'v',
-  //     width: 200,
-  //   },
-  //   {
-  //     label: 'Date',
-  //     field: 'date',
-  //     sort: 'price',
-  //   },
-  // ];
-
-
-  const handleSymbolChange = (e) => {
-    setSymbol(e);
-  };
+  const handleSortClicked = (label) => {
+    const sort = sorted === 0 || sorted === 2 ? 1 : 2;
+    let rows = datatable.rows;
+    for (let i = 0; i < 3; i ++) {
+      if (sort === 1) {
+        rows[i].sort(function (a, b) {
+          let valueA = a[label];
+          let valueB = b[label];
+          if (typeof a[label] != "string") {
+            valueA = valueA.toString();
+          }
+          if (typeof b[label] != "string") {
+            valueB = valueB.toString();
+          }
+          return valueB.localeCompare(valueA);
+        });
+      } else if (sort === 2) {
+        rows[i].sort(function (a, b) {
+          let valueA = a[label];
+          let valueB = b[label];
+          if (typeof a[label] != "string") {
+            valueA = valueA.toString();
+          }
+          if (typeof b[label] != "string") {
+            valueB = valueB.toString();
+          }
+          return valueA.localeCompare(valueB);
+        });
+      }
+    }
+    setSorted(sort);
+    setDatatable({
+      columns: datatable.columns,
+      rows,
+    })
+  }
 
   useEffect(() => {
-    const columns = data.columns.map((item, index) => {
-      if (index === 0) {
+    if (data) {
+      const columns = data.columns.map((item, index) => {
         return {
           label: item,
           field: item,
-          width: 400,
-          attributes: {
-            'aria-controls': 'DataTable',
-            'aria-label': 'BreakDown',
-          }
         };
-      } else {
-        return {
-          label: item,
-          field: item,
-          width: 150,
-        };
-      }
-    });
-
-    setDatatable({
-      columns,
-      rows: data.rows,
-    });
+      });
+  
+      setDatatable({
+        columns,
+        rows: data.rows,
+      });
+    }
   }, [data]);
 
   return (
     <div className="hunter-chart-container">
       <div className="col-sm-12 hunter-data-table-container financial-data-table">
-        <div className="hunter-search-filter-area">
-          <div className="select-option">
-            <Select
-              value={symbol}
-              onChange={handleSymbolChange}
-              options={optionsSymbol}
-              placeholder="Symbol"
-            />
-          </div>
-        </div>
         {datatable && (
           <MDBTable
             hover
@@ -103,9 +75,14 @@ const FinancialDataTable = (props) => {
             paging={false}
           >
             <MDBTableHead
-              columns={datatable.columns}
               class={"financial-table-head"}
-            />
+            >
+              <tr>
+                {datatable.columns.map((item) => (
+                  <th key={item.label} onClick={() => handleSortClicked(item.label)}>{item.label} <span><i className="fa fa-sort" /></span></th>
+                ))}
+              </tr>
+            </MDBTableHead>
             <MDBTableBody
               class={"financial-table-body-1"}
               rows={datatable.rows[0]}
