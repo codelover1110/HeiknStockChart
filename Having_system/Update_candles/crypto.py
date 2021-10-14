0c2d4d658b1dc190f4ee49884e17042bb21184e0
Alexandra <code.lover1110@gmx.com>
2021-10-14 22:50:28 +0200

    update for deploy

--- Having_system/Update_candles/crypto.py
+++ Having_system/Update_candles/crypto.py
@@ -297,7 +297,7 @@ if __name__ == "__main__":
                 time.sleep(5)
                 proc_time += 5
 
-        time.sleep(10)
+        time.sleep(1800)
     for thrd in thread_list:
         thrd.stop()
 
